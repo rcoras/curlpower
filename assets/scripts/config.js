19e87313fdee1c9e356d3320cf961b15a89804fb
Ko-stant <kostant.stanton@gmail.com>
2018-02-13 10:06:18 -0500

    Updates config info
    Updates URL for production API
    Adds URL for development API
    
    Updates index.js to have access to event handlers that will be set up
    in the respective events.js files for Auth and Surveys
    
    @rcoras
    @mimasofia

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://survey-power.herokuapp.com/',
+    development: 'http://localhost:4741'
   }
 }
 
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,16 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js')
+const surveyEvents = require('./survey/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
+})
+
+$(() => {
+  authEvents.addHandlers()
+  surveyEvents.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
