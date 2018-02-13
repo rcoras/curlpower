b08d4931df3a8bd9571b184ddc3fc8c4063e00ac
Ko-stant <kostant.stanton@gmail.com>
2018-02-13 11:15:44 -0500

    Adds auth event handlers
    Adds Sign-In, Sign-Up, Sign-Out, and Change Password events and their
    respective event handlers.

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+const api = require(`./api`)
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+// signs in user
+const onSignIn = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .then(api.getAllMountains)
+    .then(ui.getAllMountainsSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+}
+
+const onDocReady = function () {
+  ui.mainView()
+  ui.loadNav()
+}
+
+const addHandlers = function () {
+  $('.main').on('submit', '#sign-up-form', onSignUp)
+  $('.main').on('submit', '#sign-in-form', onSignIn)
+  $('.signed-in-nav').on('submit', '#change-password-form', onChangePassword)
+  $('.signed-in-nav').on('click', '#sign-out', onSignOut)
+  $(document).ready(onDocReady)
+}
+
+module.exports = {
+  addHandlers
+}
