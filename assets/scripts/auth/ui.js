639ace581b726e0d330b51cc3eb10fb86a37dd33
Ko-stant <kostant.stanton@gmail.com>
2018-02-18 16:02:14 -0500

    Updates Ux for proper messaging
    Edits `addMessage` function to be more universally available by allowing
    it to accept an element as a parameter so it can be used with other
    messages.
    
    Corrects some messages to display more useful information to the user
    
    @mimasofia

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -8,6 +8,7 @@ const showNavTemplate = require('../templates/load-nav.handlebars')
 const showSurveyContentTemplate = require('../templates/survey-content.handlebars')
 const loadUserActionsTemplate = require('../templates/user-actions.handlebars')
 const loadFeedbackMessageTemplate = require('../templates/feedback-message.handlebars')
+const survUi = require('../survey/ui')
 
 const clearFields = function () {
   $('input:text, input:password').val('')
@@ -18,7 +19,7 @@ const signUpSuccess = function (data) {
   const email = $('#sign-up-email').val()
   clearFields()
   $('#sign-in-email').val(email)
-  $('.status-message-sign-up').text('Account created. Please Sign In')
+  survUi.addMessage('.status-message-sign-up', 'Account Create. Please sign in.')
 }
 
 const signedInNav = function () {
@@ -62,36 +63,37 @@ const signInSuccess = function (data) {
 
 const signInFailure = function (error) {
   const errorStatus = error.status.toString()
-  if (errorStatus.startsWith('4') === true) {
-    $('.status-message-sign-in').text('Incorrect Email or Password.')
-  } else if (errorStatus.startsWith('5') === true || errorStatus.startsWith('0') === true) {
-    $('.status-message-sign-in').text('Unable to contact server.')
+  if (errorStatus.startsWith('4') === true || errorStatus.startsWith('5') === true) {
+    survUi.addMessage('.status-message-sign-in', 'Incorrect Email or Password.')
+  } else if (errorStatus.startsWith('0') === true) {
+    survUi.addMessage('.status-message-sign-in', 'Unable to contact server.')
   }
   clearFields()
 }
 
 const signUpFailure = function (error) {
+  console.log(error)
   const errorStatus = error.status.toString()
   if (errorStatus.startsWith('4') === true) {
-    $('.status-message-sign-up').text('Invalid Email or Password.')
+    survUi.addMessage('.status-message-sign-up', 'Invalid Email or Password.')
   } else if (errorStatus.startsWith('5') === true || errorStatus.startsWith('0') === true) {
-    $('.status-message-sign-up').text('Invalid Email or Password.')
+    survUi.addMessage('.status-message-sign-up', 'Invalid Email or Password.')
   }
   clearFields()
 }
 
 const changePasswordSuccess = function (data) {
-  $('.status-message-change-password').text('Successfully changed password.')
+  survUi.addMessage('.status-message-change-password', 'Successfully changed password.')
   clearFields()
 }
 
 const changePasswordFailure = function (error) {
   const errorStatus = error.status.toString()
   // console.log('cp store is', store)
-  if (errorStatus.startsWith('4') === true) {
-    $('.status-message-change-password').text('Incorrect Old Password.')
-  } else if (errorStatus.startsWith('5') === true || errorStatus.startsWith('0') === true) {
-    $('.status-message-change-password').text('Unable to contact server.')
+  if (errorStatus.startsWith('4') === true || errorStatus.startsWith('5') === true) {
+    survUi.addMessage('.status-message-change-password', 'Incorrect password combination.')
+  } else if (errorStatus.startsWith('0') === true) {
+    survUi.addMessage('.status-message-change-password', 'Unable to contact server.')
   }
   clearFields()
 }
